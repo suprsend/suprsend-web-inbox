cf0cdac0159abaa522d72aed9d071c05af972e4d
sivaram000 <katta.sivaram@suprsend.com>
2024-01-13 17:27:10 +0530

    embeddable inbox setup

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import SuprSendInbox from "@suprsend/react-inbox";
+
+export function initSuprSendInbox(targetElem, config) {
+  if (
+    !targetElem ||
+    !config?.workspaceKey ||
+    !config?.distinctId ||
+    !config?.subscriberId
+  ) {
+    return;
+  }
+
+  render(
+    <SuprSendInbox
+      workspaceKey={config.workspaceKey}
+      distinctId={config.distinctId}
+      subscriberId={config.subscriberId}
+      tenantId={config?.tenantId}
+      stores={config?.stores}
+      pageSize={config?.pageSize}
+      pagination={config?.pagination}
+      themeType={config?.themeType}
+      hideInbox={config?.hideInbox}
+      hideToast={config?.hideToast}
+      notificationClickHandler={config?.notificationClickHandler}
+      popperPosition={config?.popperPosition}
+      theme={config?.theme}
+      toastProps={config?.toastProps}
+    />,
+    targetElem
+  );
+}
+
+export function clearSuprSend() {
+  const targetElem = document.getElementById("suprsend-inbox");
+  if (!targetElem) return;
+  unmountComponentAtNode(targetElem);
+}
+
+if (window?.suprSendConfig) {
+  const targetElem = document.getElementById("suprsend-inbox");
+  const config = window.suprSendConfig || {};
+  initSuprSendInbox(targetElem, config);
+}
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const PACKAGE = require("./package.json");
+
+module.exports = (env) => {
+  let buildType = env.type;
+  let output;
+
+  if (buildType === "browser") {
+    output = {
+      path: path.resolve(__dirname, "dist-cdn"),
+      filename: `v${PACKAGE.version}.js`,
+      clean: true,
+      library: {
+        name: "SuprSend",
+        type: "window",
+      },
+    };
+  } else if (buildType === "module") {
+    output = {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      clean: true,
+      library: { name: "SuprSend", type: "umd" },
+    };
+  } else if (buildType === "esm") {
+    output = {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.esm.js",
+      library: { type: "module" },
+    };
+  }
+
+  return {
+    entry: "./src/index.js",
+    output: output,
+    mode: "production",
+    experiments: {
+      outputModule: buildType === "esm",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+      alias: {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+      },
+    },
+  };
+};
